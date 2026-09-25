70e8794f420bdee45e85df19b95ad8834e14ca1a
Franyx <franyx2001@gmail.com>
2026-09-25 17:46:40 +0200

    Set versionName to 0.1.0-alpha

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,7 +14,7 @@ android {
         minSdk = 35
         targetSdk = 37
         versionCode = 1
-        versionName = "1.0"
+        versionName = "0.1.0-alpha"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
